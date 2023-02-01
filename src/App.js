34d30458d6f36d16b906b90ddcd9e7aca506c659
Patrick <kelly.patj@gmail.com>
2023-02-01 10:26:16 -0500

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+        <Route path='/about' element={ <About /> } />
+        <Route path='/contact' element={ <Contact /> } />
+        <Route path='/projects' element={ <Projects /> } />
+        <Route path='/resume' element={ <Resume /> } />
+      </Routes>
+    </>
   );
 }
 

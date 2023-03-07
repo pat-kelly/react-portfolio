de40041c4baa921e34d49eb5f1ad59dbc7d7eb1f
Patrick <kelly.patj@gmail.com>
2023-03-07 17:24:28 -0500

    adjusts landing page styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,8 @@ const Home = () => {
       <div className={styles.hero}>
         <div className={styles.heroLeft}>
           <h1>Meet Patrick</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aliquam illo cumque officiis asperiores</p>
+          <div>Full Stack Developer</div>
+          <div>Motorcycle Enthusiast</div>
         </div>
         <div className={styles.heroRight}>
           <img className={styles.headShot} alt='patrick' src='https://i.imgur.com/izJwDia.png'></img>

9a8a133e72430d8af764aff44a45c4ab034bd938
Patrick <kelly.patj@gmail.com>
2023-03-07 18:03:50 -0500

    adjusts headshot tooltip and removes testing margin

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,8 @@ const Home = () => {
           <div>Motorcycle Enthusiast</div>
         </div>
         <div className={styles.heroRight}>
-          <img className={styles.headShot} alt='patrick' src='https://i.imgur.com/izJwDia.png'></img>
+          <img className={styles.headShot} alt='patrick' src='https://i.imgur.com/izJwDia.png' title='I love taking my motorcycle on cross-contry road trips!'/>
+          {/* TODO: change this to a pretty css popup */}
         </div>
       </div>
     </div>
